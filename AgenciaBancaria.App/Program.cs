7f87f8440a8b55407c52f9f70871acba2351e739
JMAURICIORLIMA <mauricio.rodriguesdelima@gmail.com>
2021-09-09 11:06:11 -0300

    Primeiro Commit

---
+++ AgenciaBancaria.App/Program.cs
@@ -0,0 +1,52 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************************************");
+            Console.WriteLine("      BEM VINDO AO DIO - BANKING");
+            Console.WriteLine("");
+            
+            try
+            {
+                    Endereco endereco = new Endereco("R: J. B. da C. A.", "53050-140", "Jd.Atântico", "Olinda", "PE");
+                    Cliente cliente = new Cliente("José Maurício", "12312312312", "1233211", endereco);
+
+                    ContaCorrente conta = new ContaCorrente(cliente, 100);
+                    Console.WriteLine("Conta - " + conta.Situacao + "\n" + conta.NumeroConta + " - " + conta.DigitoVerificador);
+
+                    string senha = "Abc@@123456";
+                    conta.Abrir(senha);
+
+                    Console.WriteLine("Conta - " + conta.Situacao + "\n" + conta.NumeroConta + " - " + conta.DigitoVerificador);
+
+                    conta.Sacar(10, senha);
+                    Console.WriteLine("Saldo: " + conta.Saldo);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("SELECIONE A CONTA DESEJADA: ");
+            Console.WriteLine("P- CONTA POUPANÇA: ");
+            Console.WriteLine("C- CONTA CORRENTE: ");
+            Console.WriteLine("DIGITE UMA DAS OPÇÕES ABAIXO: ");
+            Console.WriteLine("1- VERIFICAR SALDO");
+            Console.WriteLine("2- SACAR");
+            Console.WriteLine("4- DEPOSITAR");
+            Console.WriteLine("X- SAIR");
+            //string OpCliente = Console.ReadLine(OpCliente);
+
+
+
+
+
+        }
+    }
+}
---
+++ AgenciaBancaria.Dominio/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public class Cliente //Conceito de emcapsulamento
+    {
+        public Cliente(string nome, string cpf, string rg, Endereco endereco)
+        {
+            /*if(string.IsNullOrWhiteSpace(nome)) Verifica se a string nome está vazia.
+            {                                  se válido, execulta uma excessão.
+                throw new Exception("Campo *Nome* deve está preenchido corretamente. ");
+            }*/
+
+            Nome = nome.ValidaStringVazia();
+            CPF = cpf.ValidaStringVazia();
+            RG = rg.ValidaStringVazia();
+            Endereco = endereco ?? throw new Exception("Endereço deve ser informado. ");
+        }
+
+        public string Nome { get; private set; } //Propriedade pode ser usada como variável.
+        public string CPF { get; private set; }
+        public string RG { get; private set; }
+        public Endereco Endereco { get; private set; }
+        
+        
+    }
+}
